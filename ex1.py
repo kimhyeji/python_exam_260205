b4ef5626aa6ae674827ec3c13882839b0cf8daa2
kimhyeji <gpwl5589@gmail.com>
2026-02-06 18:16:42 +0900

    사용자 정의 모듈

---
+++ ex1.py
@@ -0,0 +1,14 @@
+def func1():
+    return "모듈에 있는 첫 번째 함수"
+
+
+def func2():
+    return "모듈에 있는 두 번째 함수"
+
+
+def func3():
+    return "모듈에 있는 세 번째 함수"
+
+
+def func4():
+    return "모듈에 있는 네 번째 함수"
